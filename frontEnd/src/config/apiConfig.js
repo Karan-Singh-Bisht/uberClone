0768821aadeaa8105d4805203d556330428b6c3c
Karan <karansbisht7@gmail.com>
2025-06-21 11:04:15 +0530

    Minor changes

--- frontEnd/src/config/apiConfig.js
+++ frontEnd/src/config/apiConfig.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "http://localhost:3000";
-// export const API_BASE_URL = "https://quickryde.onrender.com";
+// export const API_BASE_URL = "http://localhost:3000";
+export const API_BASE_URL = "https://quickryde.onrender.com";
 // export const API_BASE_URL = "https://uberclone-sxk3.onrender.com";
